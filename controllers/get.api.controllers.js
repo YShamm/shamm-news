7bd378647f1d3b9e7eb82095227db25e93ca762a
Admin <macbookair2021@Macbooks-MacBook-Air.local>
2023-08-16 12:04:36 +0100

    test GET /api with ammendements

--- controllers/get.api.controllers.js
+++ controllers/get.api.controllers.js
@@ -2,10 +2,8 @@ const { fetchAllTopics } = require("../models/get.api.models");
 const endpoints = require("../endpoints.json");
 
 exports.getTopics = (request, response, next) => {
-  console.log(request.body, "<<<req.bod in controller");
   fetchAllTopics()
     .then((topics) => {
-      console.log(topics, "control topics");
       response.status(200).send({ topics });
     })
     .catch(next);
